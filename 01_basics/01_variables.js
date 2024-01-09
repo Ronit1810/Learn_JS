1eca462fc0495a992e4f69751586fbdb7c89d981
RONIT PATEL <ronitpatel1810@gmail.com>
2024-01-09 21:23:25 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const id = "101547";
+let email = "ronit@.com";
+var password = "ronit456";
+city = "nagpur";
+
+console.table([id, email, password, city]);
+/* In above example 'const' will be fixed but other can be changeable */
+/* In early stage of JS 'var' was used but it lack in scoping */
+/* In modern era we use 'let' */

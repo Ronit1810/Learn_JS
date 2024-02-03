aa360038d1f611cb1ddcac0387fb5e239fb38c2e
RONIT PATEL <ronitpatel1810@gmail.com>
2024-02-03 16:10:07 +0530

    object_02

--- 02_basics_(programming)/02_Objects.js
+++ 02_basics_(programming)/02_Objects.js
@@ -8,8 +8,10 @@
 
 */
 
+// Object Constructor (singleton) 
+const User = new Object()
 
-// Object Literal
+// Object Literal (non-singleton)
 const JsUser = {
     name:"ronit",
     email: "ronit@12@gmail.com",
@@ -53,3 +55,45 @@ JsUser2.greeting2 = function() {
 console.log(JsUser2.greeting2());
 
 
+
+
+console.log("--");
+/*--------------------------------------------PART-02----------------------------------------------------------*/
+
+//Creating new  Object
+const AppUser = {};
+
+//Adding value in AppUser Object
+AppUser.name = "ronit";
+AppUser.email = "ronit@gmail.com"
+
+console.log(AppUser);
+
+
+//We can also add object inside object
+const AppUser2 = {
+    name2: "ronit",
+    email2: "roni12@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Ronit",
+            lastname: "Patel",
+        }
+    }
+}
+
+console.log(AppUser2.fullname.userfullname.lastname);
+
+
+
+//we can all combine to object using Spread operator(...)
+const AppUser3 = {...AppUser, ...AppUser2};
+console.log(AppUser3);
+
+
+//we can also print/get a array of keys or values of object
+console.log(Object.keys(AppUser));
+console.log(Object.values(AppUser));//further this arrays are use by looping or mapping
+
+//we can also print/get a arrays of key-value pair of object
+console.log(Object.entries(AppUser));
